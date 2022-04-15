4109d9969698d8129f5c7add4843d7a649f21817
jimmyctrl <jimextor@gmail.com>
2022-04-14 21:38:25 -0500

    se agrego Interaccion con Dom y se desactivo anterior preentrega

--- comp2.js
+++ comp2.js
@@ -4,7 +4,7 @@ Simulador de ingresos de usuarios a un programa de vacantes
 */ 
 
 
-class vacant{
+/* class vacant{
     constructor(name, lastName, age, profession){
         this.name = name
         this.lastName = lastName
@@ -67,4 +67,28 @@ showAllvacancines();
 console.log("Nombres alfabeticamente");
 showVacants();
 console.log("Filtro con datos de las personas que se encontraron por profesion");
-showProfession();
+showProfession(); */
+
+
+///////////// MANEJO DEL DOM ///////////////  DESAFIO COMPLEMENTARIO DOM 14/04/2022
+
+const form = document.getElementById('formulario');
+
+//genero un evento atravez de la constante form y el evento submit con el fin de que se pueda enviar el formulario y validarlo
+form.addEventListener('submit', function(event){ 
+
+    //aqui se llama al evento para ver en detalle lo que esta sucediendo o en este caso ingresando por inputs
+    event.preventDefault();
+
+    //genero estas variables para indicarle al programa que estoy trabajando con los bloques de imput y los .value para seleccionar solo los datos que son ingresados, y no toda la etiqueta input con sus atributos
+    let mailnombre = document.getElementById('nombres').value
+    let mailapellido = document.getElementById('apellidos').value
+    let mailedad = document.getElementById('edad').value
+    let mailprofesion = document.getElementById('profesion').value
+    //llamo a los datos ingresados por consola
+    console.log(mailnombre);
+    console.log(mailapellido);
+    console.log(mailedad);
+    console.log(mailprofesion);
+})
+
